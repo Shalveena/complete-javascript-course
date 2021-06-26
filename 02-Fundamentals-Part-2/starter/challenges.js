f7960a7065be59ae30017c9ce1ff1108a7420f10
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-06-26 11:37:21 +1000

    finish coding challenge 1 of fundamentals part 2

---
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Coding Challenge #1
+/*
+//1. Create arrow function to calculate average of three values
+const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+
+//2. Use the function to calculate the average for both teams;
+//Data 1:
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+//3. Create "checkWinner" function to log the winner to the console together with the victory points. A team only wins if it has at least double the avg score of the other team.
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log(`There is no winner`);
+  }
+};
+
+//4. Call "checkWinner" to determine winner
+checkWinner(avgDolphins, avgKoalas);
+
+//Using test Data 2
+//Data 2;
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+checkWinner(avgDolphins, avgKoalas);
+*/
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -45,3 +45,148 @@ console.log(appleOrangeJuice);
 
 //DRY: Don't Repeat Yourself!
 */
+
+//////////////////////////////////////////////////
+// Lecture 34 Function Declarations vs Function Expressions
+/*
+//Function declaration
+function calcAge1(birthYear) {
+  return 2021 - birthYear;
+}
+
+const ageShally = calcAge1(1987);
+console.log(ageShally);
+
+//Function expression
+// since it is an expression, it produces a value, which we store in a variable
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+
+const ageMax = calcAge2(1984);
+console.log(ageMax);
+
+// main difference: we can call function declarations before they are defined. Usually not a good idea to do this though, unless really necessary
+*/
+
+//////////////////////////////////////////////////
+// Lecture 35 Arrow Functions
+/*
+//Arrow functions are a shorter form of function expression
+const calcAge3 = (birthYear) => {
+  return 2021 - birthYear;
+};
+
+//round brackets not needed when only one parameter
+// curly brackets not needed when only one line of code.
+// in that case, don't need return keyword either because of implicit return.
+const calcAge4 = (birthYear) => 2021 - birthYear;
+const age4 = calcAge4(1987);
+console.log(age4);
+
+//calculate how many years left until retirement
+const yearsUntilRetire = (birthYear, firstName) => {
+  const age = 2021 - birthYear;
+  const retirement = 67 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years.`;
+};
+
+console.log(yearsUntilRetire(1987, "Shalveena"));
+console.log(yearsUntilRetire(1984, "Max"));
+
+// fundamental difference with arrow funtions - arrow functions don't get a 'this' keyword.
+*/
+
+//////////////////////////////////////////////////
+// Lecture 36 Functions calling other functions
+/*
+// calling one function from inside another function
+// imagine fruit processor needs another machine to first cut the fruit into smaller pieces.
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples); //calling cutFruitPieces with the argument we received from fruitProcessor rather than with a number directly.
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple pieces, and ${orangePieces} orange pieces.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
+
+//////////////////////////////////////////////////
+// Lecture 37 Reviewing functions
+/*
+
+// //arrow function
+// const yearsUntilRetire = (birthYear, firstName) => {
+//   const age = 2021 - birthYear;
+//   const retirement = 67 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+
+//changed to function expression
+// const yearsUntilRetire = function (birthYear, firstName) {
+//   const age = 2021 - birthYear;
+//   const retirement = 67 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+
+//calling one function inside another function;
+// const calcAge = function (birthYear) {
+//   return 2021 - birthYear;
+// };
+
+// const yearsUntilRetire = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   return retirement;
+//   // return `${firstName} retires in ${retirement} years.`;
+// };
+
+// console.log(yearsUntilRetire(1987, "Shalveena"));
+// console.log(yearsUntilRetire(1951, "Manni"));
+
+// accounting for the situation where someone has already retired:
+
+const calcAge = function (birthYear) {
+  return 2021 - birthYear;
+};
+
+// const yearsUntilRetire = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = age < 65 ? 65 - age : "You have already retired! :)";
+//   return retirement;
+// };
+
+// const yearsUntilRetire = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const yearsToRetirement = 65 - age;
+//   const retirement =
+//     yearsToRetirement > 0 ? yearsToRetirement : "You have already retired! 😊";
+//   return retirement;
+// };
+
+//alternative:
+
+const yearsUntilRetire = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement; //return immediately exists the function
+  } else {
+    return "You have already retired! 😊";
+  }
+};
+
+console.log(yearsUntilRetire(1987, "Shalveena"));
+console.log(yearsUntilRetire(1951, "Manni"));
+*/
