368f4577ef2df5a08dbf8e6e9bb536447094fed0
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-09-24 11:14:04 +1000

    finish challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -105,6 +105,26 @@ const displayMovements = (movements) => {
 displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
+
+// Coding Challenge 1:
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  const dogAges = dogsJulia.slice(1, -2).concat(dogsKate);
+
+  dogAges.forEach((age, i) => {
+    if (age > 2) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+console.log("---- Data1 ----");
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log("---- Data2 ----");
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
 // LECTURES
 
 // const currencies = new Map([
