e720fe1929befecbaca9b2be9e494453c00627e5
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-12-15 15:29:48 +1100

    picking up where I left off, doing challenge 1 again

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -77,7 +77,7 @@ const createHTML = (movements) => {
     <div class="movements__row">
       <div class="movements__type movements__type--${typeOfTransaction}">${
       i + 1
-    } deposit</div>
+    } ${typeOfTransaction}</div>
       <div class="movements__date">3 days ago</div>
       <div class="movements__value">${mov}</div>
     </div>
@@ -166,6 +166,23 @@ console.log("---- Data1 ----");
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log("---- Data2 ----");
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+// Second try (on later date):
+/*
+const dogsJulia = [9, 16, 6, 8, 3]; // [3, 5, 2, 12, 7];
+const dogsKate = [10, 5, 6, 1, 4]; // [4, 1, 15, 8, 3];
+
+const checkDogs = (dogslist1, dogslist2) => {
+  const copyDogsList1 = dogslist1.slice(1, -2);
+  copyDogsList1.concat(dogslist2).forEach((dog, i) => {
+    dog >= 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  });
+};
+
+console.log(checkDogs(dogsJulia, dogsKate));
 */
 
 // Coding Challenge 2:
